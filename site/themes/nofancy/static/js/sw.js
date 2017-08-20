5c57939f4e86b206ef758476b5bceca37efcd059
Alexander Kushi-Willis <ackw.kushi@gmail.com>
2017-08-20 21:21:41 +0100

    added network first cache serviceworker

--- site/themes/nofancy/static/js/sw.js
+++ site/themes/nofancy/static/js/sw.js
@@ -1,1 +1,7 @@
-console.log('sw.js');
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
